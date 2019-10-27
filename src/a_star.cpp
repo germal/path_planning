baaf923668523d6175fae1bfc6261ac5f8c9ae4b
Sean Oliver <snoliver@umich.edu>
2019-10-27 16:09:13 -0400

    added a_star.cpp

---
+++ src/a_star.cpp
@@ -0,0 +1,8 @@
+bool operator==(const Node& lhs, const Node& rhs){}
+
+A_star::A_star(){}
+
+void A_star::backtracker(std::vector<Node*>& path){}
+int A_star::min_cost(const Node& node){}
+void A_star::gpsCallback(const nav_msgs::Odometry::ConstPtr& msg){}
+void A_star::costMapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg){}
